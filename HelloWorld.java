d28986a1856ed95f04c81f1839dafa4f255adabf
Phathutshedzo <mamagauphathu@gmail.com>
2026-03-12 12:20:09 +0200

    Phase 2

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ public class HelloWorld{
         String myName = "Phathutshedzo Mamagau";
         public static void main(String[] args){
                 HelloWorld name = new HelloWorld(); 
-                System.out.println("Hello" + name.myName + ", Welcome to coding Devs");
+                System.out.println("Hello " + name.myName + ", Welcome to coding in Devs");
        }
 }
